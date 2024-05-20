4c73bc67cb9ce409ecfddbf72e95adbd00ae778c
Данил Рузин <ruzin-de@yandex.ru>
2024-05-20 14:04:34 +0500

    Добавление групп

---
+++ Notes-API/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿namespace Notes_API.Models
+{
+    public class Group
+    {
+        public Group(string name)
+        {
+            Name = name;
+            CreationDate = DateTime.UtcNow;
+            Notes = new List<Note>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public ICollection<Note> Notes { get; set; }
+    }
+}
--- Notes-API/Models/Note.cs
+++ Notes-API/Models/Note.cs
@@ -2,21 +2,20 @@
 {
     public class Note
     {
-
-        public Note(string title, string description)
+        public Note(string title, string description, Guid groupId)
         {
             Title = title;
             Description = description;
             CreationDate = DateTime.UtcNow;
+            GroupId = groupId;
         }
 
-
         public Guid Id { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
 
-        public DateTime CreationDate { get; set; }  
+        public Guid GroupId { get; set; }
+        public Group Group { get; set; }
     }
 }
